78354c0042463f080658215f283c372b7d439634
Vanessa Marchetti <vanessa.marchetti@sumup.com>
2022-08-11 00:30:21 -0300

    Creates conexion with database

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	router "api/src"
+	"api/src/config"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("Rodando a API")
-
+	config.Carregar()
 	r := router.Gerar()
 
-	log.Fatalln(http.ListenAndServe(":1000", r))
+	fmt.Sprintf("Escutando na porta %d", config.Porta)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d ", config.Porta), r))
 }
---
+++ src/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"strconv"
+)
+
+var (
+	//StringConexaoBanco é a string de conexão com o MySql
+	StringConexaoBanco = ""
+
+	//Porta onde a API vai estar rodando
+	Porta = 0
+)
+
+//Carregar vai inicializar as variáveis de ambiente
+func Carregar() {
+	var erro error
+
+	if erro = godotenv.Load(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	Porta, erro = strconv.Atoi(os.Getenv("API_PORT"))
+	if erro != nil {
+		Porta = 9000
+	}
+
+	StringConexaoBanco = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USUARIO"),
+		os.Getenv("DB_SENHA"),
+		os.Getenv("DB_NOME"))
+}
