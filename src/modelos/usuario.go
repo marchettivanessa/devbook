99fc1fbc8db1998b770de345961c57f2d94a7524
Vanessa Marchetti <vanessa.marchetti@sumup.com>
2022-08-13 15:09:40 -0300

    Validate password using bcrypt

--- src/modelos/usuario.go
+++ src/modelos/usuario.go
@@ -1,6 +1,7 @@
 package modelos
 
 import (
+	"api/src/seguranca"
 	"errors"
 	"github.com/badoux/checkmail"
 	"strings"
@@ -23,7 +24,9 @@ func (usuario *Usuario) Preparar(etapa string) error {
 		return erro
 	}
 
-	usuario.formatar()
+	if erro := usuario.formatar(etapa); erro != nil {
+		return erro
+	}
 	return nil
 }
 
@@ -51,8 +54,17 @@ func (usuario *Usuario) validar(etapa string) error {
 	return nil
 }
 
-func (usuario *Usuario) formatar() {
+func (usuario *Usuario) formatar(etapa string) error {
 	usuario.Nome = strings.TrimSpace(usuario.Nome)
 	usuario.Nick = strings.TrimSpace(usuario.Nick)
 	usuario.Email = strings.TrimSpace(usuario.Email)
+
+	if etapa == "cadastro" {
+		senhaComHash, erro := seguranca.Hash(usuario.Senha)
+		if erro != nil {
+			return erro
+		}
+		usuario.Senha = string((senhaComHash))
+	}
+	return nil
 }
