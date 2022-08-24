493a7b4378fc76be23ee820debd88e4ec0682283
Vanessa Marchetti <vanessa.marchetti@sumup.com>
2022-08-24 00:36:36 -0300

    Configures cookies

--- webapp/main.go
+++ webapp/main.go
@@ -5,15 +5,17 @@ import (
 	"log"
 	"net/http"
 	"webapp/src/config"
+	"webapp/src/cookies"
 	"webapp/src/router"
 	"webapp/src/utils"
 )
 
 func main() {
 	config.Carregar()
+	cookies.Configurar()
 	utils.CarregarTemplates()
-
 	r := router.Gerar()
+
 	fmt.Printf("Rodando WebApp! Escutando na porta %d\n", config.Porta)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
