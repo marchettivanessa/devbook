03d498ca722b96973adf50b308092e66644eff88
Vanessa Marchetti <vanessa.marchetti@sumup.com>
2022-09-30 15:16:23 -0300

    Renderizando  publucação na página principal

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -1,9 +1,13 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"webapp/src/config"
+	"webapp/src/modelos"
+	"webapp/src/requisicoes"
+	"webapp/src/respostas"
 	"webapp/src/utils"
 )
 
@@ -22,8 +26,23 @@ func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
 	url := fmt.Sprintf("%s/publicacoes", config.APIURL)
 
 	//Caso a autenticação e criação do token estivesse ok:
-	//response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodGet, url, nil)
-	response, erro := http.Get(url)
-	fmt.Println(response.StatusCode, erro)
-	utils.ExecutarTemplate(w, "home.html", nil)
+	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodGet, url, nil)
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
+	}
+	defer response.Body.Close()
+	//response, erro := http.Get(url)
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeDeErro(w, response)
+		return
+	}
+
+	var publicacoes []modelos.Publicacao
+	if erro = json.NewDecoder(response.Body).Decode(&publicacoes); erro != nil {
+		respostas.JSON(w, http.StatusUnprocessableEntity, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	//fmt.Println(response.StatusCode, erro)
+	utils.ExecutarTemplate(w, "home.html", publicacoes)
 }
