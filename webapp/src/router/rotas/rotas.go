80b71ee5aa0ecf3aa4d5b9bdd79d3047b0fa7918
Vanessa Marchetti <vanessa.marchetti@sumup.com>
2022-10-09 17:32:25 -0300

    Descomentar o loop em rotas.go

--- webapp/src/router/rotas/rotas.go
+++ webapp/src/router/rotas/rotas.go
@@ -1,8 +1,10 @@
 package rotas
 
 import (
-	"github.com/gorilla/mux"
 	"net/http"
+	"webapp/src/middlewares"
+
+	"github.com/gorilla/mux"
 )
 
 // Rota representa todas as rotas da aplicação web
@@ -19,18 +21,18 @@ func Configurar(router *mux.Router) *mux.Router {
 	rotas = append(rotas, rotasUsuarios...)
 	rotas = append(rotas, rotaPaginaPrincipal)
 
-	//for _, rota := range rotas {
-	//
-	//	if rota.RequerAutenticacao {
-	//		router.HandleFunc(rota.URI,
-	//			middlewares.Logger(middlewares.Autenticar(rota.Funcao)),
-	//		).Methods(rota.Metodo)
-	//	} else {
-	//		router.HandleFunc(rota.URI,
-	//			middlewares.Logger(rota.Funcao),
-	//		).Methods(rota.Metodo)
-	//	}
-	//}
+	for _, rota := range rotas {
+
+		if rota.RequerAutenticacao {
+			router.HandleFunc(rota.URI,
+				middlewares.Logger(middlewares.Autenticar(rota.Funcao)),
+			).Methods(rota.Metodo)
+		} else {
+			router.HandleFunc(rota.URI,
+				middlewares.Logger(rota.Funcao),
+			).Methods(rota.Metodo)
+		}
+	}
 
 	for _, rota := range rotas {
 		router.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
